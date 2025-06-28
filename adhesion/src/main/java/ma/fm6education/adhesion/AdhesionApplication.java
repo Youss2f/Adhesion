11b590eff9349fcd710882ebc12fd552e092d720
Youssef <Youssef.ATERTOUR.2@OUTLOOK.COM>
2025-06-28 18:12:26 +0100

    feat: Add main Spring Boot application class

---
+++ adhesion/src/main/java/ma/fm6education/adhesion/AdhesionApplication.java
@@ -0,0 +1,12 @@
+package ma.fm6education.adhesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhesionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhesionApplication.class, args);
+	}
+}
